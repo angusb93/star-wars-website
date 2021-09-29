89e8a206c997b7398da10335d59fd08c06e43ff0
angusb93 <angusbuick@gmail.com>
2021-09-29 11:36:50 +1000

    added ability favorite films

--- components/Film.js
+++ components/Film.js
@@ -1,9 +1,15 @@
 import React from "react";
 import styles from "../styles/FilmStyles.module.scss";
+import Star from "./Star";
+
 export default function Film(props) {
+  //   const [rating, setRating] = useState();
   return (
     <div className={styles.filmCard}>
       <div className={styles.filmTitle}>{props.film.title}</div>
+      <div className={styles.filmStars}>
+        <Star filled={props.favorited} onClick={props.onClick} />
+      </div>
     </div>
   );
 }
--- components/FilmList.js
+++ components/FilmList.js
@@ -1,18 +1,40 @@
-import React from "react";
+import { React, useState } from "react";
 import useSWR from "swr";
 import axios from "axios";
 import Film from "./Film";
+
 import styles from "../styles/FilmListStyles.module.scss";
 const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 const MovieList = () => {
   const { data, error } = useSWR("https://swapi.dev/api/films/", fetcher);
-  console.log(data);
+
+  const [favorited, setFavorited] = useState({ films: [] });
+
+  const handleClick = (id) => {
+    if (favorited.films.includes(id)) {
+      setFavorited({ films: favorited.films.filter((film) => film !== id) });
+    } else {
+      setFavorited({
+        films: [...favorited.films, id],
+      });
+    }
+  };
+  // console.log(data);
+  console.log(favorited);
   if (error) return "An error has occurred.";
   if (!data) return "Loading...";
+
   //iterate over the data and return a list of films
   const films = data.results.map((film) => {
-    return <Film key={film.episode_id} film={film} />;
+    return (
+      <Film
+        key={film.episode_id}
+        film={film}
+        favorited={favorited.films.includes(film.episode_id) ? true : false}
+        onClick={() => handleClick(film.episode_id)}
+      />
+    );
   });
 
   return <div className={styles.filmContainer}>{films}</div>;
